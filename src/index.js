c6032a48df74085f76d8e5c9f387b1c1b8274b99
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-06 15:35:08 -0400

    feedback

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /** GENERAL FEEDBACK:
- *
+ * Great job getting all test cases to pass! Love the use of FormData! 
  */
 
 const handleSubmit = () => {
+  /** FEEDBACK: This line of code should be outside, or in a runner function! Hint: main() */
     const form = document.getElementById('new-user-form');
-
+    /** FEEDBACK: The code in the callback function that is in the event handler should be inside of the function handleSubmit!  */
     form.addEventListener('submit', (stuff) => {
         stuff.preventDefault(); 
 
