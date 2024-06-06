cc3db31fcbc63d37cb2347280554475920378e2d
Taifinkaba <taifinkabad@gmail.com>
2024-06-06 15:24:03 -0400

    eh

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@
  */
 
 const handleSubmit = () => {
+    const form = document.getElementById('new-user-form');
+
+    form.addEventListener('submit', (stuff) => {
+        stuff.preventDefault(); 
+
+        const formData = new FormData(stuff.target);
+        const { username, codingLevel, location, didLikeAssignment } = Object.fromEntries(formData);
+
+        document.getElementById('results-username').textContent = username;
+        document.getElementById('results-coding-level').textContent = codingLevel;
+        document.getElementById('results-location').textContent = location;
+        document.getElementById('results-did-like-assignment').textContent = didLikeAssignment === 'on' ? 'Yes' : 'No';
+
+        stuff.target.reset();
+    });
 };
+
+handleSubmit(); 
